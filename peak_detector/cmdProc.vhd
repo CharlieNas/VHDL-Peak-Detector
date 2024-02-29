c857a466bbc3d536fc0ea0991737b162bc7ba1f3
joshfeather <qh22044@bristol.ac.uk>
2024-02-29 15:28:18 +0000

    Added command processor entity

--- peak_detector/cmdProc.vhd
+++ peak_detector/cmdProc.vhd
@@ -1,1 +1,26 @@
 -- TEAM B 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.common_pack.all;
+
+entity cmdProc is
+port (
+    clk:		in std_logic;
+    reset:        in std_logic;
+    rxnow:        in std_logic;
+    rxData:            in std_logic_vector (7 downto 0);
+    txData:            out std_logic_vector (7 downto 0);
+    rxdone:        out std_logic;
+    ovErr:        in std_logic;
+    framErr:    in std_logic;
+    txnow:        out std_logic;
+    txdone:        in std_logic;
+    start: out std_logic;
+    numWords_bcd: out BCD_ARRAY_TYPE(2 downto 0);
+    dataReady: in std_logic;
+    byte: in std_logic_vector(7 downto 0);
+    maxIndex: in BCD_ARRAY_TYPE(2 downto 0);
+    dataResults: in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
+    seqDone: in std_logic
+    );
+end cmdProc;
