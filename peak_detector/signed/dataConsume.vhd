ffed6ba55f4703ebfb05600bd65006429207f067
damianstone <damianstonedev@gmail.com>
2024-03-27 13:10:52 +0000

    fix simulation warnings

--- peak_detector/signed/dataConsume.vhd
+++ peak_detector/signed/dataConsume.vhd
@@ -3,6 +3,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 use work.common_pack.all;
 
 --------------- ENTITY DECLARATION -------------------------------------------------------------------------------------------------
@@ -23,17 +24,19 @@ ENTITY dataConsume IS
     byte: out std_logic_vector(7 downto 0); -- send the number to the command processor after process the number (S2)
     maxIndex: out BCD_ARRAY_TYPE(2 downto 0); -- index of the largest number (peak) in dataResults
     dataResults: out CHAR_ARRAY_TYPE(0 to 6) -- array of 7 numbers (bytes) with the largest one in the middle
-  )
+  );
 END dataConsume;
 
 ------------------------------------------------------------------------------------------------------------------------------------
 
 architecture Behavioral of dataConsume is
+  -------------------------------------------------------------  TYPES  ----------------------------------------------------------------
   type state_type is (S0, S1, S2, S3);
-  signal curr_state, next_state: state_type;
+  type signed_array is array (integer range <>) of signed(7 downto 0);
 
   -------------------------------------------------------------  SIGNALS  ----------------------------------------------------------------
-
+  signal curr_state, next_state: state_type;
+  
   signal BCD2int_enable: boolean := FALSE;
   signal numWords_int: integer := 0;
   signal counter: integer := 0;
@@ -44,8 +47,8 @@ architecture Behavioral of dataConsume is
   signal compare_enable: boolean := FALSE;
   signal peak_index: integer := 0;
   signal peak_found: integer := 0; -- set to 3 and decrements if peak found, in order to record next three bytes
-  signal currentBytes: array(0 to 6) of signed(7 downto 0); -- acts as our main buffer
-  signal lastThreeBytes: array(0 to 2) of signed(7 downto 0); -- always storing last three bytes
+  signal currentBytes: signed_array(0 to 6); -- acts as our main buffer
+  signal lastThreeBytes: signed_array(0 to 2); -- always storing last three bytes
 
   signal max_index_bcd_enable: boolean := FALSE;
   signal max_index_bcd: BCD_ARRAY_TYPE(2 downto 0); 
@@ -79,13 +82,13 @@ architecture Behavioral of dataConsume is
   StoreInDataResult: process(clk)
   begin
     if store_data_result_enable then
-      dataResults(0) <= currentBytes(0);
-      dataResults(1) <= currentBytes(1);
-      dataResults(2) <= currentBytes(2);
-      dataResults(3) <= currentBytes(3); -- peak
-      dataResults(4) <= currentBytes(4);
-      dataResults(5) <= currentBytes(5);
-      dataResults(6) <= currentBytes(6);
+      dataResults(0) <= std_logic_vector(currentBytes(0));
+      dataResults(1) <= std_logic_vector(currentBytes(1));
+      dataResults(2) <= std_logic_vector(currentBytes(2));
+      dataResults(3) <= std_logic_vector(currentBytes(3)); -- peak
+      dataResults(4) <= std_logic_vector(currentBytes(4));
+      dataResults(5) <= std_logic_vector(currentBytes(5));
+      dataResults(6) <= std_logic_vector(currentBytes(6));
     end if;
   end process;
 
@@ -96,11 +99,11 @@ architecture Behavioral of dataConsume is
           -- 1. Update buffer with next three values if peak was recent
           if peak_found > 0 then
             if peak_found = 3 then
-              currentBytes(4) = current_value;
+              currentBytes(4) <= current_value;
             elsif peak_found = 2 then
-              currentBytes(5) = current_value;
+              currentBytes(5) <= current_value;
             elsif peak_found = 1 then
-              currentBytes(6) = current_value;
+              currentBytes(6) <= current_value;
             peak_found <= peak_found - 1;
             end if;
           end if;
@@ -132,7 +135,7 @@ architecture Behavioral of dataConsume is
 
   ByteOutput: process(clk)
   begin
-      if rising_edge(clk) and dataReady = '1' then
+      if rising_edge(clk) then
           byte <= data; -- Output the processed data
       end if;
   end process;
@@ -165,14 +168,24 @@ architecture Behavioral of dataConsume is
           currentBytes <= (others => (others => '0'));
           lastThreeBytes <= (others => (others => '0'));
           max_index_bcd_enable <= FALSE;
-          max_index_bcd <= (others => 'X');
+          max_index_bcd(2) <= (others => 'X');
+          max_index_bcd(1) <= (others => 'X');
+          max_index_bcd(0) <= (others => 'X');
           store_data_result_enable <= FALSE;
 
           -- reset from the entity
           seqDone <= '0';
           dataReady <= '0';                      
-          dataResults <= (others => 'X');
-          maxIndex <= (others => 'X');
+          dataResults(0) <= (others => 'X');
+          dataResults(1) <= (others => 'X');
+          dataResults(2) <= (others => 'X');
+          dataResults(3) <= (others => 'X');
+          dataResults(4) <= (others => 'X');
+          dataResults(5) <= (others => 'X');
+          dataResults(6) <= (others => 'X');
+          maxIndex(2) <= (others => 'X');
+          maxIndex(1) <= (others => 'X');
+          maxIndex(0) <= (others => 'X');
 
 
           if start = '1' then
@@ -212,28 +225,8 @@ architecture Behavioral of dataConsume is
           seqDone <= '1';
           next_state <= S0;
 
-
         when others =>
-          -- default reseting
-          -- reset created structures
-          BCD2int_enable <= FALSE;
-          counter <= 0;
-          current_value <= (others => '0');
-          peak_value <= (others => '0');
-          compare_enable <= 0;
-          peak_index <= 0;
-          peak_found <= 0;
-          currentBytes <= (others => (others => '0'));
-          lastThreeBytes <= (others => (others => '0'));
-          max_index_bcd_enable <= FALSE;
-          max_index_bcd <= (others => 'X');
-          store_data_result_enable <= FALSE;
-
-          -- reset from the entity
-          seqDone <= '0';
-          dataReady <= '0';                      
-          dataResults <= (others => 'X');
-          maxIndex <= (others => 'X');
+          -- default reseting 
       end case;
 
   end process;
